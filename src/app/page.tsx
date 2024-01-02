e050c2eea00c49e0f5b6123a95b3613c4b631852
ChaseRensberger <chasehrensberger@gmail.com>
2024-01-02 10:54:08 -0500

    display json data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useState } from "react";
 import { CardSides } from "./lib/utils";
+import Cards from "@/cards.json";
 export default function Home() {
   const [cardSide, setCardSide] = useState(CardSides.Front);
+  const [currentCard, setCurrentCard] = useState<number>(0);
 
   const toggleCardSide = () => {
     if (cardSide) {
@@ -11,13 +13,23 @@ export default function Home() {
       setCardSide(CardSides.Back);
     }
   };
+
+  const nextCard = () => {
+    setCurrentCard(Math.floor(Math.random() * Cards.length));
+  };
   return (
     <main className="bg-black text-white min-h-screen">
-      {cardSide === CardSides.Front ? <dd>Question</dd> : <dt>Answer</dt>}
+      {cardSide === CardSides.Front ? (
+        <dd>{Cards[currentCard].question}</dd>
+      ) : (
+        <dt>{Cards[currentCard].answer}</dt>
+      )}
       <button className="p-2 rounded-md bg-blue-500" onClick={toggleCardSide}>
         +
       </button>
-      <button className="p-2 rounded-md bg-blue-500">Next</button>
+      <button className="p-2 rounded-md bg-blue-500" onClick={nextCard}>
+        Next
+      </button>
     </main>
   );
 }
